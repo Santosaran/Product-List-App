69edd661b178f18279dd560ee15a7915eba1a6ec
Saran <113179649+Santosaran@users.noreply.github.com>
2024-01-29 23:18:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react'; 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
-import {useState,useEffect} from 'react';
+import {useState,useEffect} from 'react'; 
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home'; 
